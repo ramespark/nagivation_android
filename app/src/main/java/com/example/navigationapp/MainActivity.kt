e127d1f080bca29cb9bd6d47b5e85fb85eeb8a61
Rames Palanisamy <rames.p@gmail.com>
2025-02-21 00:11:35 +0530

    Add initial Android navigation app code

---
+++ app/src/main/java/com/example/navigationapp/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.navigationapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import com.example.navigationapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+    }
+}
